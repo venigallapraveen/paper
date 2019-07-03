8ea9f3661059594c8f2fdbeb097b4c3534ff3ae6
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-03 17:29:14 -0400

    added a navbar with signup, home and login buttons

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+//import from components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
+//material imports
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+import Button from "@material-ui/core/Button";
+
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <AppBar color="default">
+        <ToolBar>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Signup</Button>
+        </ToolBar>
+      </AppBar>
     );
   }
 }
