99cbd52f1a3c7d5d979cd27e4e205c5112ba53e5
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-03 18:13:32 -0400

    added material ui color theme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import customTheme from "./util/customTheme";
 
 //import from pages
 import home from "./pages/home";
@@ -11,21 +14,25 @@ import signup from "./pages/signup";
 //import from components
 import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+const theme = createMuiTheme(customTheme);
+
+class App extends React.Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/util/customTheme.js
@@ -0,0 +1,16 @@
+export default {
+  palette: {
+    primary: {
+      light: "#00927c",
+      main: "#00d1b2",
+      dark: "#33dac1",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#b22743",
+      main: "#ff3860",
+      dark: "#ff5f7f",
+      contrastText: "#fff"
+    }
+  }
+};
