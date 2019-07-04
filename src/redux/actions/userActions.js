66c3bfdf06bc8a9874917782ac93281ab5589783
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-04 18:02:59 -0400

    added the user actions for loginUser

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+  MARK_NOTIFICATIONS_READ
+} from "../types";
+import axios from "axios";
+
+export const loginUser = (userData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/login", userData)
+    .then(res => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+export const getUserData = () => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .get("/user")
+    .then(res => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+const setAuthorizationHeader = token => {
+  const PaperApiToken = `Bearer ${token}`;
+  localStorage.setItem("PaperApiToken", PaperApiToken);
+  axios.defaults.headers.common["Authorization"] = PaperApiToken;
+};
