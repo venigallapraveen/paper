263ce1e0f2b4c19dd416b3f5b52d95f1526c1b18
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-03 21:00:24 -0400

    added dayjs plugin to the scream component

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,21 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 //material ui imports
 import Card from "@material-ui/core/Card";
+import withStyles from "@material-ui/core/styles/withStyles";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
 const styles = {
   card: {
-    display: "flex"
+    position: "relative",
+    display: "flex",
+    marginBottom: 10
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover"
   }
 };
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -29,14 +42,19 @@ class Scream extends Component {
       }
     } = this.props;
     return (
-      <Card>
-        <CardMedia image={""} title="Profile Image" />
-        <CardContent>
-          <Typography variant="h5" color="primary">
+      <Card className={classes.card}>
+        <CardMedia image={""} title="Profile Image" className={classes.image} />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            color="primary"
+            component={Link}
+            to={`/users/${userHandle}`}
+          >
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt._seconds}
+            {dayjs("2019-07-01").fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
