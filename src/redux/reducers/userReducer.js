239bddbf0b6f5fa0b54b2f5db3af909ea571b1fe
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-04 18:07:17 -0400

    added the userReducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import {
+  SET_USER,
+  SET_AUTHENTICATED,
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  MARK_NOTIFICATIONS_READ
+} from "../types";
+
+const initialState = {
+  authenticated: false,
+  loading: false,
+  credentials: {},
+  likes: [],
+  notifications: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_AUTHENTICATED:
+      return {
+        ...state,
+        authenticated: true
+      };
+    case SET_UNAUTHENTICATED:
+      return initialState;
+    case SET_USER:
+      return {
+        authenticated: true,
+        loading: false,
+        ...action.payload
+      };
+
+    default:
+      return state;
+  }
+}
