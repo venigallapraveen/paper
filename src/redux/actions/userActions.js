88605d5c005022ba1fdd90d661eb759a98085640
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-05 11:36:25 -0400

    added a profile skeleton template

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -59,6 +59,16 @@ export const getUserData = () => dispatch => {
     .catch(err => console.log(err));
 };
 
+export const uploadImage = formData => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .post("/user/image", formData)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => console.log(err));
+};
+
 export const logoutUser = () => dispatch => {
   localStorage.removeItem("PaperApiToken");
   delete axios.defaults.headers.common["Authorization"];
