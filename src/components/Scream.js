f3ef5ad3923bd8d18f81d83994b2495713f71e2c
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-11 18:21:28 -0400

    Added redux store to the screams

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -3,6 +3,11 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import MyButton from "../../util/MyButton";
+import DeleteScream from "./DeleteScream";
+import ScreamDialog from "./ScreamDialog";
+import LikeButton from "./LikeButton";
 
 //material ui imports
 import Card from "@material-ui/core/Card";
@@ -10,6 +15,10 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import ChatIcon from "@material-ui/icons/Chat";
+
+// Redux
+import { connect } from "react-redux";
 
 const styles = {
   card: {
@@ -39,28 +48,64 @@ class Scream extends Component {
         screamId,
         likeCount,
         commentCount
+      },
+      user: {
+        authenticated,
+        credentials: { handle }
       }
     } = this.props;
+
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeleteScream screamId={screamId} />
+      ) : null;
     return (
       <Card className={classes.card}>
-        <CardMedia image={""} title="Profile Image" className={classes.image} />
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
         <CardContent className={classes.content}>
           <Typography
             variant="h5"
-            color="primary"
             component={Link}
             to={`/users/${userHandle}`}
+            color="primary"
           >
             {userHandle}
           </Typography>
+          {deleteButton}
           <Typography variant="body2" color="textSecondary">
-            {dayjs("2019-07-01").fromNow()}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
+          <LikeButton screamId={screamId} />
+          <span>{likeCount} Likes</span>
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} comments</span>
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
         </CardContent>
       </Card>
     );
   }
 }
 
-export default withStyles(styles)(Scream);
+Scream.propTypes = {
+  user: PropTypes.object.isRequired,
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(Scream));
