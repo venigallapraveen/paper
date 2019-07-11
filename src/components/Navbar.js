8a6cbb816872467b6d18d7d460a31e9091f6a22f
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-11 15:57:32 -0400

    added authentication on signin and redux store to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
 
 //material imports
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import Button from "@material-ui/core/Button";
+import HomeIcon from "@material-ui/icons/Home";
 
-export class Navbar extends Component {
+class Navbar extends Component {
   render() {
+    const { authenticated } = this.props;
     return (
       <AppBar color="default">
         <ToolBar className="nav-container">
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-          <Button color="inherit" component={Link} to="/signup">
-            Signup
-          </Button>
+          {authenticated ? (
+            <Fragment>
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon />
+                </MyButton>
+              </Link>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </Fragment>
+          )}
         </ToolBar>
       </AppBar>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
